ef6d5f9457a50790fc5aa4de375dd9a1f43832a1
Kenneth Armstrong <kmarmstr@ualberta.ca>
2013-03-01 12:01:11 -0700

    Added ingredient, partially implemented pantry (2)

---
+++ src/ca/ualberta/cs/oneclick_cookbook/Pantry.java
@@ -0,0 +1,30 @@
+package ca.ualberta.cs.oneclick_cookbook;
+
+import java.util.ArrayList;
+
+public class Pantry {
+	private ArrayList<Ingredient> ingredientList;
+	
+	Pantry() {
+		
+	}
+	
+	public void addIngredient(Ingredient i) {
+		ingredientList.add(i);
+	}
+	
+	public boolean removeIngredient(String name) {
+		for (int i=0; i<ingredientList.size(); i++) {
+			Ingredient ingredient = (Ingredient) ingredientList.get(i);
+
+			if (ingredient.getName() == name) {
+				ingredientList.remove(ingredient);
+				return true;
+			}
+		}
+		// This happens if name wasn't in the list
+		return false;
+	}
+	
+	
+}
