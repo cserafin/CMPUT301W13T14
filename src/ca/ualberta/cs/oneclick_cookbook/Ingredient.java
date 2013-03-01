c412951fd9ee2074b7193d499d1babb954962537
Kenneth Armstrong <kmarmstr@ualberta.ca>
2013-03-01 11:58:58 -0700

    Added ingredient, partially implemented pantry.
    I added the getters/setters and constructor for ingredient, added add/remove
    functionality to pantry.

---
+++ src/ca/ualberta/cs/oneclick_cookbook/Ingredient.java
@@ -0,0 +1,36 @@
+package ca.ualberta.cs.oneclick_cookbook;
+
+public class Ingredient {
+	private int quantity;
+	private String measurement;
+	private String name;
+	
+	Ingredient (String name, int quantity, String measurement) {
+		this.name = name;
+		this.quantity = quantity;
+		this.measurement = measurement;
+	}
+	
+	public void modifyQuantity (int quantity, String measurement) {
+		this.quantity = quantity;
+		this.measurement = measurement;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public String getMeasurement() {
+		return measurement;
+	}
+	
+	// This should probably be modified to return a string
+	public void printIngredient() {
+		return;
+	}
+
+}
