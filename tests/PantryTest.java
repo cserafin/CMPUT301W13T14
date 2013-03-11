b487e65e19fe5c42130bd276c1e3e153ea5050f4
Kenneth Armstrong <kmarmstr@gmail.com>
2013-03-11 11:28:39 -0600

    Added some more JUnit tests.

--- tests/PantryTest.java
+++ tests/PantryTest.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import org.junit.*;
 import static org.junit.Assert.*;
 import ca.ualberta.cs.oneclick_cookbook.Ingredient;
@@ -86,9 +88,48 @@ public class PantryTest {
         p.addIngredient(new Ingredient("Flour", 42, "Pounds"));
         p.addIngredient(new Ingredient("Bacon", 13, "Crates"));
         p.addIngredient(new Ingredient("Toast", 200, "Slices"));
-        assertTrue("Test: pantrySearch1", p.isInPantry("Flour"));
+        assertTrue("Test:pantrySearch:Test 1", p.isInPantry("Flour"));
+        assertTrue("Test:pantrySearch:Test 2", p.isInPantry("Toast"));
         p.emptyPantry();
-        assertFalse("Test: pantrySearch2", p.isInPantry("Flour"));
+        assertFalse("Test:pantrySearch:Test 3", p.isInPantry("Flour"));
+    }
+    
+    //Test the pantry string array function
+    @Test
+    public void testStringArrayList() {
+    	ArrayList<String> s = new ArrayList<String>();
+    	p.addIngredient(new Ingredient("Flour", 3, "mL"));
+    	s.add(p.get(0).toString());
+    	p.addIngredient(new Ingredient("Bread", 3, "mL"));
+    	s.add(p.get(1).toString());
+    	p.addIngredient(new Ingredient("Salt", 4, "teaspoons"));
+    	s.add(p.get(2).toString());
+    	assertEquals("Pantry:stringarrayList:Test 1", p.getStringArrayList(), s);
+    }
+    
+    // Test the toLower function of pantry
+    @Test
+    public void testToLower() {
+    	p.addIngredient(new Ingredient("Flour", 3, "mL"));
+    	p.addIngredient(new Ingredient("BrEAD", 3, "CuPs"));
+    	p.addIngredient(new Ingredient("SALt", 3, "ITemS"));
+    	p.toLower();
+    	assertTrue("Pantry:toLower:Test 1", p.isInPantry("bread"));
+    	assertFalse("Pantry:toLower:Test 2", p.isInPantry("sALt"));
+    }
+    
+    // Test the toString function of pantry
+    @Test
+    public void testToString() {
+    	String s = "";
+    	p.addIngredient(new Ingredient("Flour", 3, "mL"));
+    	s += p.get(0).toString() + ", ";
+    	p.addIngredient(new Ingredient("Bread", 3, "mL"));
+    	s += p.get(1).toString() + ", ";
+    	p.addIngredient(new Ingredient("Salt", 4, "teaspoons"));
+    	s += p.get(2).toString();
+    	System.out.println(p.toString());
+    	assertEquals("Pantry:toString:Test 1", s, p.toString());
     }
                    
 }
